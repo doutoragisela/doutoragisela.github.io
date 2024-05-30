292c407099bad8d69f216b8a48bd476b528f9134
Doutora Gisela Otorrino <doutoragiselaotorrino@gmail.com>
2024-05-30 01:06:01 -0300

    Initial commit

---
+++ assets/js/index.js
@@ -0,0 +1,35 @@
+(function ($) {
+  $(document).ready(function(){
+    const scrollToAnchor = (id) => {
+      let elem = $(id);
+      $('html,body').animate({scrollTop: elem.offset().top - 120}, 'slow');
+    };
+
+    $(function () {
+      $(".health-clinic-1-wa").click((e) => {
+        e.preventDefault();
+        location.href = "https://wa.me/5511972042759";
+      });
+
+      $(".health-clinic-2-wa").click((e) => {
+        e.preventDefault();
+        location.href = "https://wa.me/551138490543";
+      });
+
+      $(".about-button").click((e) => {
+        e.preventDefault();
+        scrollToAnchor("#aboutSection");
+      });
+
+      $(".gallery-button").click((e) => {
+        e.preventDefault();
+        scrollToAnchor("#gallerySection");
+      });
+
+      $(".contact-button").click((e) => {
+        e.preventDefault();
+        scrollToAnchor("#contactSection");
+      });
+    });
+  });
+}(jQuery));
